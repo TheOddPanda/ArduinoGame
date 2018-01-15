566e8792ddf2cbc20043bc0d4be6adeabfbafb5b
5ia15adbuechi <5ia15adbuechi@gibwintra.ch>
2018-01-15 14:58:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ArduinoGame
+This is a game which is made for a school project.
+
+#Contributors @abuechi @abajra
---
+++ game/build/.default/-.d
@@ -0,0 +1,2 @@
+.sc9155107509307076011.o: \
+ D:\Schule\3.\ Lehrjahr\M242\MikrocomputerSimulator\ArduinoWorkspace\Game\.sc9155107509307076011.cpp
